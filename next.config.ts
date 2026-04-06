7ecd95dee48e3e9348137ec79062c9c7f5573ab9
Damocles <damocles@presage.market>
2026-04-06 11:04:42 +0200

    Add favicon config in next.config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    favicon: "/favicon.ico",
+  },
 };
 
 export default nextConfig;
